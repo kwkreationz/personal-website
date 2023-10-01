75d34b35ea55e0823ca3085674a38c9067a1df38
kwkreationz <kwkreationz@gmail.com>
2023-09-30 23:46:34 -0400

    project10update

--- script.js
+++ script.js
@@ -42,3 +42,23 @@ previousImage.addEventListener('click', function() {
 nextImage.addEventListener('click', function() {
   slideRight();
 });
+
+const items = document.querySelectorAll('.item:not(:first-child)');
+
+const options = {
+  threshold: 0.5
+}
+
+function addSlideIn(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    }
+  });
+}
+
+const observer = new IntersectionObserver(addSlideIn, options)
+
+items.forEach(item => {
+  observer.observe(item);
+})
